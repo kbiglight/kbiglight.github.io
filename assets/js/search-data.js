868c796cc5304876f84b3301cf22dc46caf70c8e
kbiglight <11326307+kbiglight@users.noreply.github.com>
2025-12-17 13:12:17 +0000

    Deploying to gh-pages from @ kbiglight/kbiglight.github.io@f377137b46d5c8fced141e309451bdadba828b0e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,14 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%6B%62%69%67%6C%69%67%68%74@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-rss',
@@ -523,13 +516,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
